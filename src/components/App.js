5543b253030ee9766d2632fbb9b641d46ea2206f
zeitun <zeytunmahadi718@gmail.com>
2024-07-18 06:20:22 +0300

    fetching data

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+    const [ dogimgurl , setdogimgurl] = useState('')
+    const [isloading , setloading] = useState(true)
+    
+    useEffect (()=> {
+      
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then((response) => (response.json()))
+        .then(data => {
+            setdogimgurl(data.message);
+            setloading(false);
+        });
+
+     
+}, []);
+        
+    
+return (
+    <div>
+        {isloading ? (
+            <p>Loading...</p>
+        ) : (
+            <img src={dogimgurl} alt="A Random Dog" />
+        )}
+    </div>
+);
+}
